69a5c22ffd85a6a843e253c96e4a568d4540712e
Draxer98 <150442304+Draxer98@users.noreply.github.com>
2025-02-28 18:01:41 +0100

    Update Main.java

--- Gara_Dadi/src/Main.java
+++ Gara_Dadi/src/Main.java
@@ -1,31 +1,27 @@
-import frontEnd.Dado;
+import frontEnd.Gara;
 import frontEnd.Giocatore;
-import frontEnd.Partita;
 
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        Dado dado = new Dado();
-        Partita partita = new Partita();
-        System.out.println("Inserisci il nome G1: ");
-        String nomeG1 = scanner.nextLine();
-        Giocatore G1 = new Giocatore(nomeG1);
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("Nome giocatore 1: ");
+        String nome1 = keyboard.nextLine();
+        System.out.println("Nome giocatore 2: ");
+        String nome2 = keyboard.nextLine();
+        System.out.println("Quanti round si desidera effettuare: ");
+        int nRound = Integer.parseInt(keyboard.nextLine());
 
-        System.out.println("Inserisci il nome G2: ");
-        String nomeG2 = scanner.nextLine();
-        Giocatore G2 = new Giocatore(nomeG2);
+        Gara gara = new Gara(nome1, nome2, nRound);
 
-        System.out.println("Quanti round vuoi fare: ");
-        int round = Integer.parseInt(scanner.nextLine());
-
-        String vincitore = partita.Round(round, G1, G2, dado);
-
-        System.out.println("Il vincitore è: " + vincitore);
-        System.out.println(G1);
-        System.out.println(G2);
-
-        partita.ResetGame(G1, G2);
+        do {
+            System.out.println(gara.StatoPartita());
+            System.out.println(gara.giocaRound());
+            if (gara.giocaRound().equals("Round minimi richiesti: 3")) {
+                break;
+            }
+        } while (gara.StatoPartita().equals("In corso"));
+        System.out.println(gara.Winner());
     }
 }
